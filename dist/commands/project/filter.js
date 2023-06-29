55883f66a8d0f11b066c19c938616eca8ebe5afb
bornostojak <borno.stojak@gmail.com>
2023-06-29 20:58:46 +0200

    removing dist/ from git

--- dist/commands/project/filter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*
-TODO: write a filter function that can filter through exiting projects
-TODO: looking for specific service version, etc.
-TODO: add switch "-a, --activate" to activate those projects
-*/ 
-//# sourceMappingURL=filter.js.map
